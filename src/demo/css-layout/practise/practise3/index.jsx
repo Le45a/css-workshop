eda1c04fd9c6b1e20023dc2844b6a5821b2ba9c0
yingzhang <yingzhang@thoughtworks.com>
2022-04-13 13:22:43 +0800

    [feat] add xxx

--- src/demo/css-layout/practise/practise3/index.jsx
+++ src/demo/css-layout/practise/practise3/index.jsx
@@ -21,18 +21,6 @@ const Practise03 = () => {
         前往练习页面
       </a>
       <div className={styles.image} />
-      {/*<div style={{display: 'flex', height: '400px'}}>*/}
-      {/*    <div style={{backgroundColor: '#366AFF', width: '200px', padding: '16px', color: '#FFF'}}>sider</div>*/}
-      {/*    <div style={{flexGrow: 1, display: 'flex', flexDirection: 'column'}}>*/}
-      {/*        <div style={{borderBottom: '1px solid #6B7280',display: "flex", justifyContent: 'space-between', padding: '16px'}}>*/}
-      {/*            <div>首页</div>*/}
-      {/*            <div>设置</div>*/}
-      {/*        </div>*/}
-      {/*        <div style={{padding: '16px', flexGrow: 1, overflow: "auto"}}>*/}
-      {/*            <div style={{backgroundColor:'#D1D5DB', height: '100vh', padding: '16px'}}>我是高度为100vh内容</div>*/}
-      {/*        </div>*/}
-      {/*    </div>*/}
-      {/*</div>*/}
     </>
   );
 };
